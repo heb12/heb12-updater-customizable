9e1b6c4f64e56c797f0c611ddb3f6d204a17b424
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-23 17:10:26 -0400

    Update apiLib.js

--- apiLib.js
+++ apiLib.js
@@ -1,6 +1,6 @@
 
             $.noConflict
-            function require(url) {
+            function apiLib(url) {
                 var rtrn
              jQuery.ajax({async:false,url: url, success: function(result){
                 if(url.search(".json")!==-1) {rtrn= JSON.parse(result);} else if(url.search(".js")!==-1) {rtrn= eval("function() {"+result+"}");} else {
