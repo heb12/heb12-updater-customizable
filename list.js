a62f150b6a604618df4e8fc32effcac3a3ad13bb
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-23 17:29:29 -0400

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,8 @@
-function list() {
-
+function list(arr,hrefmodel) {
+document.body.innerHTML += "<ul></ul>";
+  var ul = document.getElementsByTagName("ul");
+  var ul = ul[ul.length];
+  for (var i=0; i<arr.length; i++) {
+  ul.innerHTML += '<li><a "'+eval(hrefmodel)+'" download="Heb12-Updater">'+arr[i]+"</a></li>";
+  }
 }
