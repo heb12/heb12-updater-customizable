f4dff163d8a8a5c12dac2edc913a1867c2d920c8
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-23 17:25:24 -0400

    Update update.js

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 if (update_) {
-list(updates.split(recent_updates)[1].split("|"),"https://heb12.ml/heb12-updateer-customizable/"+updates.split(recent_updates)[1].split("|")+".bat");
+list(updates.split(recent_updates)[1].split("|"),'"https://heb12.ml/heb12-updateer-customizable/"+updates.split(recent_updates)[1].split("|")[i]+".bat"');
 } else {
 location.assign("index.html");
 }
