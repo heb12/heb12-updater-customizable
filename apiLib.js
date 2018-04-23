c685fd7ede33aaa1f8ddfc051c3acff391c42f39
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-23 17:09:28 -0400

    Create apiLib.js

---
+++ apiLib.js
@@ -0,0 +1,10 @@
+
+            $.noConflict
+            function require(url) {
+                var rtrn
+             jQuery.ajax({async:false,url: url, success: function(result){
+                if(url.search(".json")!==-1) {rtrn= JSON.parse(result);} else if(url.search(".js")!==-1) {rtrn= eval("function() {"+result+"}");} else {
+            rtrn= result;
+                }
+        }});return rtrn;
+            }
