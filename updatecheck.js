0a4ae1b320fba7d619fa66d2ad4caa0b336c8dd1
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-23 17:16:55 -0400

    Create updatecheck.js

---
+++ updatecheck.js
@@ -0,0 +1,9 @@
+var updates =  apiLib("https://heb12.ml/heb12-updater-customizable/updates.txt");
+var recent_updates = apiLib("updates_recent.txt");
+var list = recent_updates.split(updates);
+if (list.length>1) {
+let updates_ = false;
+}
+if ("list.length===1") {
+let updates_ = true;
+}
